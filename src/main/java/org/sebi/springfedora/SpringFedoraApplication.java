4dfd5f6ce434d8416e03429b6398d15a6bffc56f
Sebastian Stoff <sebastianstoff@gmx.at>
2022-04-06 11:28:33 +0200

    #1. init commit spring setup

---
+++ src/main/java/org/sebi/springfedora/SpringFedoraApplication.java
@@ -0,0 +1,13 @@
+package org.sebi.springfedora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFedoraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFedoraApplication.class, args);
+	}
+
+}
