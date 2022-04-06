810a1f4525ebc5c41af54a75cbf7ce7170e2ce57
Sebi <SebastianStoff@gmx.at>
2022-04-06 20:21:25 +0200

    #3. moved request mapping to class definition

--- src/main/java/org/sebi/springfedora/controller/DigitalObjectController.java
+++ src/main/java/org/sebi/springfedora/controller/DigitalObjectController.java
@@ -3,9 +3,14 @@ package org.sebi.springfedora.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping({
+  "/{pid}", 
+  "objects/{pid}"
+})
 public class DigitalObjectController {
   
 
@@ -14,10 +19,7 @@ public class DigitalObjectController {
    * @param pid
    * @return
    */
-  @GetMapping({
-    "/{pid}", 
-    "/objects/{pid}"
-  })
+  @GetMapping
   @ResponseBody
   public String getDO(@PathVariable("pid") String pid){
     return pid;
