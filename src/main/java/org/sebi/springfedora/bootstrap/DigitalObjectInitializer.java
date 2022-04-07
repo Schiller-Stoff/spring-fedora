c17ea13e80fa1e7f56fa8c90405af738ec0d45ef
Sebi <SebastianStoff@gmx.at>
2022-04-07 11:24:30 +0200

    #3. injecting now digital object repo via spring

--- src/main/java/org/sebi/springfedora/bootstrap/DigitalObjectInitializer.java
+++ src/main/java/org/sebi/springfedora/bootstrap/DigitalObjectInitializer.java
@@ -1,5 +1,6 @@
 package org.sebi.springfedora.bootstrap;
 
+import org.sebi.springfedora.repository.IDigitalObjectRepository;
 import org.sebi.springfedora.service.IDigitalObjectService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Profile;
@@ -12,12 +13,13 @@ import lombok.extern.slf4j.Slf4j;
 @Component // tells spring to be a spring bean.
 public class DigitalObjectInitializer implements CommandLineRunner {
   
-  //private final IGEOSPARQLService rdf4JService;
+  private final IDigitalObjectRepository dObjectRepository;
   private final IDigitalObjectService DOService;
 
 
-  public DigitalObjectInitializer(IDigitalObjectService DOService) {
+  public DigitalObjectInitializer(IDigitalObjectService DOService, IDigitalObjectRepository dObjectRepository) {
      this.DOService = DOService;
+     this.dObjectRepository = dObjectRepository;
   }
 
   @Override
@@ -25,6 +27,10 @@ public class DigitalObjectInitializer implements CommandLineRunner {
     log.debug("Bootstraping geo data...");
     //rdf4JService.startServer();
     //rdf4JService.loadTestData();
+  }
+
+  private void initConnections(){
+    
   }
 
 }
