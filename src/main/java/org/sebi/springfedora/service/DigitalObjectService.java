70e13683a968f02a453afcea890ba14dcc802585
Sebi <SebastianStoff@gmx.at>
2022-04-07 11:23:47 +0200

    #3. added service layer

--- src/main/java/org/sebi/springfedora/service/DigitalObjectService.java
+++ src/main/java/org/sebi/springfedora/service/DigitalObjectService.java
@@ -1,10 +1,17 @@
 package org.sebi.springfedora.service;
 
 import org.sebi.springfedora.model.DigitalObject;
+import org.sebi.springfedora.repository.IDigitalObjectRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DigitalObjectService implements IDigitalObjectService {
+	
+  private IDigitalObjectRepository digitalObjectRepository;	
+	
+  public DigitalObjectService(IDigitalObjectRepository digitalObjectRepository) {
+	  this.digitalObjectRepository = digitalObjectRepository;
+  }
 
   @Override
   public DigitalObject createDigitalObject(String pid) {
@@ -15,6 +22,7 @@ public class DigitalObjectService implements IDigitalObjectService {
   @Override
   public DigitalObject getDigitalObject(String pid) {
     // TODO Auto-generated method stub
+	this.digitalObjectRepository.findById(pid);
     return null;
   }
 
