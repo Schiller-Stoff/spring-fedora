25be545fae4ff47e5b4f0c6b6b1ffd9ac768b078
Sebi <SebastianStoff@gmx.at>
2022-04-07 12:10:06 +0200

    #3. Added get resource metho

--- src/main/java/org/sebi/springfedora/repository/ResourceRepository.java
+++ src/main/java/org/sebi/springfedora/repository/ResourceRepository.java
@@ -1,7 +1,15 @@
 package org.sebi.springfedora.repository;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.Optional;
 
+import org.apache.commons.io.IOUtils;
+import org.fcrepo.client.FcrepoClient;
+import org.fcrepo.client.FcrepoOperationFailedException;
+import org.fcrepo.client.FcrepoResponse;
+import org.fcrepo.client.GetBuilder;
 import org.sebi.springfedora.model.Resource;
 import org.springframework.stereotype.Repository;
 
@@ -9,8 +17,16 @@ import org.springframework.stereotype.Repository;
 public class ResourceRepository implements IResourceRepository {
 
   @Override
-  public <S extends Resource> S save(S entity) {
-    // TODO Auto-generated method stub
+  public <S extends Resource> S save(S resource) {
+    URI uri = null;
+    try {
+      uri = new URI(resource.getUri());
+    } catch (URISyntaxException e) {
+      System.out.println("Malformed URI!");
+    }
+
+    // TODO add more
+
     return null;
   }
 
@@ -22,7 +38,32 @@ public class ResourceRepository implements IResourceRepository {
 
   @Override
   public Optional<Resource> findById(String id) {
-    // TODO Auto-generated method stub
+    URI uri = null;
+    try {
+      uri = new URI("http://localhost:8082/rest/image-collection");
+    } catch (Exception e) {
+      return null;
+    }
+
+    // FcrepoClient client = FcrepoClient.client().build();
+
+    try (
+        final FcrepoClient client = FcrepoClient.client().build();
+        FcrepoResponse response = new GetBuilder(uri, client)
+            .accept("application/rdf+xml")
+            .perform()) {
+
+      String turtleContent = IOUtils.toString(response.getBody(), "UTF-8");
+      System.out.println(turtleContent);
+
+    } catch (IOException e) {
+
+      e.printStackTrace();
+    } catch (FcrepoOperationFailedException e1) {
+
+      e1.printStackTrace();
+    }
+
     return null;
   }
 
@@ -53,33 +94,31 @@ public class ResourceRepository implements IResourceRepository {
   @Override
   public void deleteById(String id) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void delete(Resource entity) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void deleteAllById(Iterable<? extends String> ids) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void deleteAll(Iterable<? extends Resource> entities) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void deleteAll() {
     // TODO Auto-generated method stub
-    
+
   }
-  
-  
 
 }
