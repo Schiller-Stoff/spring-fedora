808ae24aec26d40f94b3c05b20dc251fbd57e776
Sebi <SebastianStoff@gmx.at>
2022-04-07 15:45:37 +0200

    #3. added missing code

--- src/main/java/org/sebi/springfedora/model/cm/TEI.java
+++ src/main/java/org/sebi/springfedora/model/cm/TEI.java
@@ -3,5 +3,10 @@ package org.sebi.springfedora.model.cm;
 import org.sebi.springfedora.model.DigitalObject;
 
 public class TEI extends DigitalObject {
+
+  public TEI(String pid, String rdf) {
+    super(pid, rdf);
+    //TODO Auto-generated constructor stub
+  }
   
 }
