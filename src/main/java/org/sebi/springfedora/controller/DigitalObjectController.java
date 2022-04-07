36b8b098833659e63f92d5965208f2f77945ae7e
Sebi <SebastianStoff@gmx.at>
2022-04-07 11:25:46 +0200

    #3. using spring service to receive a fedora resource

--- src/main/java/org/sebi/springfedora/controller/DigitalObjectController.java
+++ src/main/java/org/sebi/springfedora/controller/DigitalObjectController.java
@@ -1,5 +1,7 @@
 package org.sebi.springfedora.controller;
 
+import org.sebi.springfedora.service.DigitalObjectService;
+import org.sebi.springfedora.service.IDigitalObjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +22,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 })
 public class DigitalObjectController {
   
+  private IDigitalObjectService digitalObjectService;
+  
+  public DigitalObjectController(IDigitalObjectService digitalObjectService) {
+	  this.digitalObjectService = digitalObjectService;
+  }
+	
 
   /**
    * Default getters for GAMS objects
@@ -29,6 +37,7 @@ public class DigitalObjectController {
   @GetMapping
   @ResponseBody
   public String getDO(@PathVariable("pid") String pid){
+	this.digitalObjectService.getDigitalObject(pid);
     return pid;
   }
 
