1fdea341396b914e76910ecb035b37ae34f16494
Sebi <SebastianStoff@gmx.at>
2022-04-07 11:25:16 +0200

    #3. added very basic implementation for retrieving a fedora resource

--- src/main/java/org/sebi/springfedora/repository/DigitalObjectRepository.java
+++ src/main/java/org/sebi/springfedora/repository/DigitalObjectRepository.java
@@ -1,11 +1,19 @@
 package org.sebi.springfedora.repository;
 
+import java.io.IOException;
+import java.net.URI;
 import java.util.Optional;
 
+import org.apache.commons.io.IOUtils;
+import org.fcrepo.client.FcrepoClient;
+import org.fcrepo.client.FcrepoOperationFailedException;
+import org.fcrepo.client.FcrepoResponse;
+import org.fcrepo.client.GetBuilder;
 import org.sebi.springfedora.model.DigitalObject;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-public class DigitalObjectRepository implements IDigitalObjectRepository, CrudRepository<DigitalObject, String> {
+@Repository
+public class DigitalObjectRepository implements IDigitalObjectRepository {
 
   @Override
   public <S extends DigitalObject> S save(S entity) {
@@ -21,7 +29,34 @@ public class DigitalObjectRepository implements IDigitalObjectRepository, CrudRe
 
   @Override
   public Optional<DigitalObject> findById(String id) {
-    // TODO Auto-generated method stub
+
+    URI uri = null;
+    try {
+      uri = new URI("http://localhost:8082/rest/image-collection");
+    } catch (Exception e){
+      return null;
+    }
+    
+    //FcrepoClient client = FcrepoClient.client().build();
+
+    try (
+        final FcrepoClient client = FcrepoClient.client().build();
+        FcrepoResponse response = new GetBuilder(uri, client)
+          .accept("application/rdf+xml")
+          .perform()
+    ) {
+
+      String turtleContent = IOUtils.toString(response.getBody(), "UTF-8");
+      System.out.println(turtleContent);
+      
+    } catch (IOException e) {
+		
+		e.printStackTrace();
+	} catch (FcrepoOperationFailedException e1) {
+		
+		e1.printStackTrace();
+	}
+
     return null;
   }
 
@@ -52,33 +87,31 @@ public class DigitalObjectRepository implements IDigitalObjectRepository, CrudRe
   @Override
   public void deleteById(String id) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void delete(DigitalObject entity) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void deleteAllById(Iterable<? extends String> ids) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void deleteAll(Iterable<? extends DigitalObject> entities) {
     // TODO Auto-generated method stub
-    
+
   }
 
   @Override
   public void deleteAll() {
     // TODO Auto-generated method stub
-    
+
   }
-  
-  
 
 }
