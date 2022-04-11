7713f9e8bd54014a5ae60981e09ee01fe41650ff
Sebastian Stoff <sebastianstoff@gmx.at>
2022-04-11 16:11:18 +0200

    #7. added logic to interface

--- src/main/java/org/sebi/springfedora/service/IDigitalObjectService.java
+++ src/main/java/org/sebi/springfedora/service/IDigitalObjectService.java
@@ -7,5 +7,7 @@ public interface IDigitalObjectService {
   public DigitalObject createDigitalObjectByPid(String pid);
   public DigitalObject createDigitalObjectByPid(String pid, String rdf);
   public DigitalObject findDigitalObjectByPid(String pid); 
+  
+  public DigitalObject deleteDigitalObjectByPid(String pid); 
 
 }
