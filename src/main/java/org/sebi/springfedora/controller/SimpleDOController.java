3bdcb4fc711a35fab77d7287d85c6a98c9b45534
Sebastian Stoff <sebastianstoff@gmx.at>
2022-04-11 19:00:10 +0200

    #8. removed unused imports

--- src/main/java/org/sebi/springfedora/controller/SimpleDOController.java
+++ src/main/java/org/sebi/springfedora/controller/SimpleDOController.java
@@ -2,11 +2,9 @@ package org.sebi.springfedora.controller;
 
 import org.sebi.springfedora.model.DigitalObject;
 import org.sebi.springfedora.service.IDigitalObjectService;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/objects/")
