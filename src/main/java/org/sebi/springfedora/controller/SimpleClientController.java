060b787a72b89771eb4601cfd46f8fb7012e6235
Sebastian Stoff <sebastianstoff@gmx.at>
2022-04-19 14:48:16 +0200

    #9. added endpoints for simple client

--- src/main/java/org/sebi/springfedora/controller/SimpleClientController.java
+++ src/main/java/org/sebi/springfedora/controller/SimpleClientController.java
@@ -1,12 +1,17 @@
 package org.sebi.springfedora.controller;
 
 import org.sebi.springfedora.model.DigitalObject;
+import org.sebi.springfedora.model.Resource;
 import org.sebi.springfedora.service.IDigitalObjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @RequestMapping("/client/")
 @Controller
@@ -30,9 +35,23 @@ public class SimpleClientController {
     return "digitalObject/show";
   }
 
+  @PostMapping("/createObject")
+  public String postObject(@RequestParam String pid, Model model){
+
+    DigitalObject digitalObject = digitalObjectService.createDigitalObjectByPid(pid);
+    model.addAttribute("do", digitalObject);
+    return "redirect:/client/" + digitalObject.getPid();
+  }
+
   @GetMapping
   public String getIndex(){
     return "index";
+  }
+
+  @GetMapping("/createObject")
+  public String createObject(Model model){
+    model.addAttribute("do", new DigitalObject("testpid", new Resource("", "")));
+    return "digitalObject/create";
   }
 
 }
