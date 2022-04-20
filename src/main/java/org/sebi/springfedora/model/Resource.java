c178b9a3ea1650dc6317f4ab71175c35738bad57
Sebi <SebastianStoff@gmx.at>
2022-04-20 07:48:59 +0200

    #14. added binary field

--- src/main/java/org/sebi/springfedora/model/Resource.java
+++ src/main/java/org/sebi/springfedora/model/Resource.java
@@ -5,8 +5,12 @@ import org.springframework.util.MimeType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
+/**
+ * Resource in terms of LDP.
+ * One of four types - here used BasicContainer / Binary
+ * Dependent if binary datastream is used.
+ */
 @Data
-@AllArgsConstructor
 public class Resource {
     // path used by fedora - abstract REST display 
     private String path;
@@ -16,4 +20,19 @@ public class Resource {
     
     // mimetype of contained datastream
     private MimeType mimeType;
+
+    // optional binary described by resource
+    private byte[] binary; 
+
+    public Resource(String path, String rdfXml, MimeType mimeType){
+        this.path = path;
+        this.rdfXml = rdfXml;
+        this.mimeType = mimeType;
+    }
+
+    public Resource(String path, String rdfXml, MimeType mimeType, byte[] binary){
+        this(path, rdfXml, mimeType);
+        this.binary = binary;
+    }
+
 }
