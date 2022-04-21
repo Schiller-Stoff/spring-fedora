761421f145745d732e1dca95e8b099151da5ee5d
Sebi <SebastianStoff@gmx.at>
2022-04-21 12:50:14 +0200

    #14. updated service interface

--- src/main/java/org/sebi/springfedora/service/IDatastreamService.java
+++ src/main/java/org/sebi/springfedora/service/IDatastreamService.java
@@ -1,7 +1,9 @@
 package org.sebi.springfedora.service;
 
+import org.sebi.springfedora.exception.ResourceRepositoryException;
 import org.sebi.springfedora.model.Datastream;
 
 public interface IDatastreamService {
   public Datastream findById();
+  public Datastream createById(String id, String mimetype) throws ResourceRepositoryException;
 }
