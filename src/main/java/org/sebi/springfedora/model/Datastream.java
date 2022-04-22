3306b96320cc7301f0201506a75a6870962aeae7
Sebastian Stoff <sebastianstoff@gmx.at>
2022-04-22 16:43:29 +0200

    #14. added required getter for content

--- src/main/java/org/sebi/springfedora/model/Datastream.java
+++ src/main/java/org/sebi/springfedora/model/Datastream.java
@@ -1,13 +1,20 @@
 package org.sebi.springfedora.model;
 
 import org.springframework.util.MimeType;
+import org.springframework.web.multipart.MultipartFile;
 
-
+import lombok.Data;
 
 public class Datastream extends Resource {
   //private String dsId; 
+  private byte[] content;
 
-  public Datastream(String path, String rdfXml, MimeType mimeType){
+  public Datastream(String path, String rdfXml, MimeType mimeType, byte[] content){
     super(path, rdfXml, mimeType);
+    this.content = content;
+  }
+
+  public byte[] getContent(){
+    return this.content;
   }
 }
