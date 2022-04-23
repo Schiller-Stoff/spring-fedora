f9b859f02d6463435e2d75219d3a9da0e1ca24f2
Sebi <SebastianStoff@gmx.at>
2022-04-23 11:16:57 +0200

    #17. create datastream via web client

--- src/main/java/org/sebi/springfedora/controller/SimpleClientController.java
+++ src/main/java/org/sebi/springfedora/controller/SimpleClientController.java
@@ -1,7 +1,11 @@
 package org.sebi.springfedora.controller;
 
+import java.io.IOException;
+
+import org.sebi.springfedora.model.Datastream;
 import org.sebi.springfedora.model.DigitalObject;
 import org.sebi.springfedora.model.Resource;
+import org.sebi.springfedora.service.IDatastreamService;
 import org.sebi.springfedora.service.IDigitalObjectService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,16 +17,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 @RequestMapping("/client/")
 @Controller
 public class SimpleClientController {
 
   IDigitalObjectService digitalObjectService;
+  IDatastreamService datastreamService;
 
 
-  public SimpleClientController(IDigitalObjectService digitalObjectService){
+  public SimpleClientController(IDigitalObjectService digitalObjectService, IDatastreamService datastreamService){
     this.digitalObjectService = digitalObjectService;
+    this.datastreamService = datastreamService;
   }
   
 
@@ -53,6 +60,21 @@ public class SimpleClientController {
   public String createObject(Model model){
     model.addAttribute("do", new DigitalObject("testpid", new Resource("", "",MimeType.valueOf("text/turtle"))));
     return "digitalObject/create";
+  }
+
+  @PostMapping("/createDatastream")
+  public String createDatastream(@RequestParam String pid, @RequestParam String dsid, @RequestParam MultipartFile file, Model model) throws IOException {
+
+    byte[] fileAsBytes = file.getBytes();
+    String mimetype = file.getContentType();
+
+    Datastream datastream = datastreamService.createById(dsid, mimetype, pid, fileAsBytes);
+    DigitalObject digitalObject = digitalObjectService.findDigitalObjectByPid(pid);
+
+    model.addAttribute("do", digitalObject);
+    model.addAttribute("datastream", datastream);
+
+    return "redirect:/client/" + digitalObject.getPid();
   }
 
 }
