14e89da0d10dd8a0f966dff4c022b3153a0f05d0
Sebastian Stoff <sebastianstoff@gmx.at>
2022-04-25 19:29:55 +0200

    #23. updated startup scripts

--- fedora/scripts.sh
+++ fedora/scripts.sh
@@ -4,6 +4,9 @@
 # downloadable from github!
 #
 
+# first cd to fedora location
+cd C:\Users\stoffse\Desktop\fedora
+
 # with full path on windows
 # !adapt if on other machine!
 java -Dfcrepo.namespace.registry=C:\\Users\\stoffse\\Desktop\\fedora\\custom_namespaces.yml -jar fcrepo-webapp-6.1.1-jetty-console.jar
