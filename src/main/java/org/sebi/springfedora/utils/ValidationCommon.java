1ea37533c99109ef31dcc7dd7431473c06f69663
Sebastian Stoff <sebastianstoff@gmx.at>
2022-05-03 18:59:27 +0200

    #16. added some validation for datastream ids

--- src/main/java/org/sebi/springfedora/utils/ValidationCommon.java
+++ src/main/java/org/sebi/springfedora/utils/ValidationCommon.java
@@ -4,6 +4,10 @@ public class ValidationCommon {
 
   public static final String VALID_PID_REGEX = "^(o|context|query|corpus|cirilo):([a-z0-9])([-._a-z0-9]){1,}$";
 
-  public static final String INVALID_PID_MESSAGE = "PID of digital object does not conform to required pattern!";
+  public static final String INVALID_PID_MESSAGE = "PID of digital object does not conform to required pattern: " + VALID_PID_REGEX;
   
+  public static final String VALID_DATASTREAM_ID = "^[a-zA-Z0-9äöüÄÖÜ_]*$";
+
+  public static final String INVALID_DATASTREAM_ID_MESSAGE = "Dastream id does not conform to required pattern: " + VALID_DATASTREAM_ID;
+
 }
