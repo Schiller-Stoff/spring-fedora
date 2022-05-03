9775d9d5bf17ed17a606b6feefda798881772f77
Sebastian Stoff <sebastianstoff@gmx.at>
2022-05-03 19:30:09 +0200

    #16. added validation on a domain class = model

--- src/main/java/org/sebi/springfedora/model/DigitalObject.java
+++ src/main/java/org/sebi/springfedora/model/DigitalObject.java
@@ -1,8 +1,29 @@
 package org.sebi.springfedora.model;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
+import org.sebi.springfedora.utils.ValidationCommon;
+import org.springframework.validation.annotation.Validated;
+
+@Validated
 public class DigitalObject extends Resource {
   
+  /**
+   * PID of a digital object as defined in OAIS.
+   */
+  @NotBlank
+  @Pattern(
+    regexp = ValidationCommon.VALID_PID_REGEX, 
+    message = ValidationCommon.INVALID_DATASTREAM_ID_MESSAGE)
   private String pid;
+
+  public DigitalObject(){
+    super(null, "");
+  }
+
+  public DigitalObject(String pid){
+    super(null, "");
+  }
   
   public DigitalObject(String pid, String path, String rdfXml){
     super(path, rdfXml);
