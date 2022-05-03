3c32d3ddb83a70d127b82897504b0f1e08f2753c
Sebastian Stoff <sebastianstoff@gmx.at>
2022-05-03 19:01:00 +0200

    #16. datastreams are now not allowed to have diacritics

--- src/main/java/org/sebi/springfedora/utils/ValidationCommon.java
+++ src/main/java/org/sebi/springfedora/utils/ValidationCommon.java
@@ -6,7 +6,7 @@ public class ValidationCommon {
 
   public static final String INVALID_PID_MESSAGE = "PID of digital object does not conform to required pattern: " + VALID_PID_REGEX;
   
-  public static final String VALID_DATASTREAM_ID = "^[a-zA-Z0-9äöüÄÖÜ_]*$";
+  public static final String VALID_DATASTREAM_ID = "^[a-zA-Z0-9_]*$";
 
   public static final String INVALID_DATASTREAM_ID_MESSAGE = "Dastream id does not conform to required pattern: " + VALID_DATASTREAM_ID;
 
