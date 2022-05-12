058db4887d3e4083583dd93e114c447d5785c4c1
Sebi <SebastianStoff@gmx.at>
2022-05-12 10:53:53 +0200

    #26. disabling security mechanism when in dev setup

---
+++ src/main/java/org/sebi/springfedora/security/ApplicationSecurity.java
@@ -0,0 +1,23 @@
+package org.sebi.springfedora.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Class configures security for local develompent
+ * - disables all security for dev profile
+ */
+@Profile( value = {"dev"})
+@Configuration
+public class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+  
+
+  @Override
+  public void configure(WebSecurity web) throws Exception {
+    web.ignoring().antMatchers("/**");
+  }
+
+}
