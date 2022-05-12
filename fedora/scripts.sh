c81861fc4c1ebd60651cf2d5b7cc3966fbb2b77b
Sebi <SebastianStoff@gmx.at>
2022-05-12 09:22:45 +0200

    #26. added demo scripts for keycloak

--- fedora/scripts.sh
+++ fedora/scripts.sh
@@ -10,3 +10,15 @@ cd C:\Users\stoffse\Desktop\fedora
 # with full path on windows
 # !adapt if on other machine!
 java -Dfcrepo.namespace.registry=C:\\Users\\stoffse\\Desktop\\fedora\\custom_namespaces.yml -jar fcrepo-webapp-6.1.1-jetty-console.jar
+
+
+
+#
+# keycloak
+#
+
+cd C:\Users\stoffse\Desktop\keycloak\keycloak-18.0.0
+
+# windows:
+# launches keycloak dev server on port 8182 on this machine
+bin\kc.bat start-dev --http-port 8182
